10b594c21b23c049de6f2a82eedbb818d40febcf
Eshu tomar <eshutomar1@gmial.com>
2025-07-27 12:04:48 +0530

    role based authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const verifyToken = require('../middleware/authMiddleware'); // Middleware to verify JWT token
+const authorizeRole = require('../middleware/roleMiddleware');
 const router = express.Router();
 
 //only admin can access this route
-router.get('/admin', verifyToken, (req, res) => {
+router.get('/admin', verifyToken, authorizeRole("admin"), (req, res) => {
   res.json({ message: 'Admin access granted' });
 });
 
 
 //both admin and manager can access this route
-router.get('/manager', verifyToken, (req, res) => {
+router.get('/manager', verifyToken, authorizeRole("admin", "manager"), (req, res) => {
   res.json({ message: 'Admin and manager access granted' });
 });
 
 
 //all  can access this route
-router.get('/user', verifyToken, (req, res) => {
+router.get('/user', verifyToken,authorizeRole("admin", "manager", "user"), (req, res) => {
   res.json({ message: 'All users access granted' });
 });
 
